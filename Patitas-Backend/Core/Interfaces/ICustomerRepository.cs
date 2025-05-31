fcffb6d3c797883feb9a62fe0b384b0ec00c511a
yerco-sanchez <yerco2695@gmail.com>
2025-05-31 18:28:31 -0400

    feat(h05-b1): add controller and repository for GET /customers

---
+++ Patitas-Backend/Core/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using Patitas_Backend.Core.Entities;
+
+namespace Patitas_Backend.Core.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task<IEnumerable<Customer>> GetAllAsync();
+}
---
+++ Patitas-Backend/Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patitas_Backend.Core.Entities;
+using Patitas_Backend.Core.Interfaces;
+using Patitas_Backend.Infrastructure.Data;
+
+namespace Patitas_Backend.Infrastructure.Repositories;
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly DataContext _context;
+
+    public CustomerRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+
+}
